f174c22db705bbc1e8c5d7a727875b2c3081d5f6
czj <624278325@qq.com>
2023-03-12 21:56:54 +0800

    1.更新

--- src/main/resources/shell/jenkins-deploy-docker.sh
+++ src/main/resources/shell/jenkins-deploy-docker.sh
@@ -1,9 +1,35 @@
 #!/usr/bin/env bash
+API_NAME=''
+API_VERSION=''
+API_PORT=''
+
+while read line
+do
+  config=(${line//=/ })
+  if [[ ${config[0]} == "name" ]]
+  then
+          echo ${config[1]}
+    API_NAME=${config[1]}
+  elif [[ ${config[0]} == "version" ]]
+  then
+          echo ${config[1]}
+    API_VERSION=${config[1]}
+  else
+    echo ${config[1]}
+    API_PORT=${config[1]}
+  fi
+
+  if [[ -n ${API_NAME} ]] && [[ -n ${API_VERSION} ]] && [[ -n ${API_PORT} ]]
+  then
+    break
+  fi
+done <<< "$(cat ../release-note)"
+
 # 配置jdk路径
 export JAVA_HOME=/home/dev/jdk-11.0.17
 # 配置jenkins映射本地workspace路径，最终路径为项目bin的启动脚本位置
-cd /home/docker/jenkins/workspace/base-boot/target/classes/shell
-cp ../../base-boot-1.0.0.jar base-boot-1.0.0.jar
+cd /home/docker/jenkins/workspace/$API_NAME/target/classes/shell
+cp ../../$API_NAME-$API_VERSION.jar $API_NAME-$API_VERSION.jar
 # -t 表示指定镜像仓库名称/镜像名称:镜像标签 .表示使用当前目录下的Dockerfile
-docker build -t base-boot/base-boot:1.0.0 .
-docker stop base-boot && docker rm base-boot && docker run -p 10004:10002 --name base-boot -v /home/logs/baseboot:/home/logs -d base-boot/base-boot:1.0.0
+docker build -t $API_NAME/$API_NAME:$API_VERSION .
+docker stop $API_NAME && docker rm $API_NAME && docker run -p $API_PORT:$API_PORT --name $API_NAME -v /home/logs/$API_NAME:/home/logs -d $API_NAME/$API_NAME:$API_VERSION
--- src/main/resources/shell/jenkins-deploy.sh
+++ src/main/resources/shell/jenkins-deploy.sh
@@ -1,7 +1,32 @@
 #!/usr/bin/env bash
+API_NAME=''
+API_VERSION=''
+API_PORT=''
+
+while read line
+do
+  config=(${line//=/ })
+  if [[ ${config[0]} == "name" ]]
+  then
+          echo ${config[1]}
+    API_NAME=${config[1]}
+  elif [[ ${config[0]} == "version" ]]
+  then
+          echo ${config[1]}
+    API_VERSION=${config[1]}
+  else
+    echo ${config[1]}
+    API_PORT=${config[1]}
+  fi
+
+  if [[ -n ${API_NAME} ]] && [[ -n ${API_VERSION} ]] && [[ -n ${API_PORT} ]]
+  then
+    break
+  fi
+done <<< "$(cat ../release-note)"
 # 配置jdk路径
 export JAVA_HOME=/home/dev/jdk-11.0.17
 # 配置jenkins映射本地workspace路径，最终路径为项目bin的启动脚本位置
-cd /home/docker/jenkins/workspace/common-monitor/target/classes/bin
+cd /home/docker/jenkins/workspace/$API_NAME/target/classes/bin
 # 使用脚本启动项目
-sh service.sh restart common-monitor-1.0
+sh service.sh restart $API_NAME-API_VERSION
