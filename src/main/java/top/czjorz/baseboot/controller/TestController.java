00068b15dfede5545c8d8aaa979dc977b79926eb
czj <624278325@qq.com>
2023-03-13 00:02:42 +0800

    1.测试

--- src/main/java/top/czjorz/baseboot/controller/TestController.java
+++ src/main/java/top/czjorz/baseboot/controller/TestController.java
@@ -12,6 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping("/hello")
     public String helloWorld(){
-        return "Hello, World!";
+        return "Hello, World2!";
     }
 }
