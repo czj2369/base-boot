c727ca084cb9d32eb4128651fd7ab43f70a52618
czj-manjaro <czj@top.com>
2024-11-04 17:07:06 +0800

    更换部署方式

--- src/main/resources/shell/jenkins-deploy-docker.sh
+++ src/main/resources/shell/jenkins-deploy-docker.sh
@@ -6,10 +6,10 @@ API_PORT=''
 while read line
 do
   config=(${line//=/ })
-  if [[ ${config[0]} == "name" ]]
+  if [[ ${config[0]} == "API_NAME" ]]
   then
     API_NAME=${config[1]}
-  elif [[ ${config[0]} == "version" ]]
+  elif [[ ${config[0]} == "API_VERSION" ]]
   then
     API_VERSION=${config[1]}
   else
@@ -20,15 +20,15 @@ do
   then
     break
   fi
-done <<< "$(cat ../release-note)"
+done <<< "$(cat ./.env)"
 
-# 配置jdk路径
-export JAVA_HOME=/home/dev/jdk-11.0.17
-# 配置jenkins映射本地workspace路径，最终路径为项目bin的启动脚本位置
-cd /home/docker/jenkins/workspace/$API_NAME/target/classes/shell
-cp ../../$API_NAME-$API_VERSION.jar $API_NAME.jar
-# -t 表示指定镜像仓库名称/镜像名称:镜像标签 .表示使用当前目录下的Dockerfile
-docker build -t $API_NAME/$API_NAME:$API_VERSION .
+cp ./../$API_NAME-$API_VERSION.jar ./$API_NAME.jar
+mkdir lib
+cp ./../lib/* ./lib/
+
 docker stop $API_NAME
 docker rm $API_NAME
-docker run -p $API_PORT:$API_PORT --restart=always --name $API_NAME -v /home/logs/$API_NAME:/home/logs -d $API_NAME/$API_NAME:$API_VERSION
+docker rmi $API_NAME/$API_NAME:$API_VERSION
+# -t 表示指定镜像仓库名称/镜像名称:镜像标签 .表示使用当前目录下的Dockerfile
+docker build -t $API_NAME/$API_NAME:$API_VERSION .
+docker compose up -d
